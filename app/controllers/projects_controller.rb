b735aea4d8a6d9a49a663d47676817bd172d4d85
Unitato1 <robertsvihla@gmail.com.github.com>
2024-02-29 15:59:22 +0100

    Added date form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,19 @@ class ProjectsController < ApplicationController
   def index
     @todos = Project.all
   end
+  def new
+    @project = Project.new
+  end
+  def create
+    @project = Project.create(project_parms)
+    if @project.save
+      redirect_to projects_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def project_parms
+    params.require(:project).permit(:name, :description, :till)
+  end
 end
